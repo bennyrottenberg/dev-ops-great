73c050c2b68575f5dcd3d13ee0a11d8ecd4d57f7
brottenberg <brottenberg@synamedia.com>
2023-01-03 21:31:13 +0200

    rm config

--- start_system.sh
+++ start_system.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-pwd
 project_path=$(pwd)
-echo "project_path $project_path"
 cd ..
-pwd
 git clone https://github.com/bennyrottenberg/jenkins_configuration
 cd $project_path
-pwd
 docker build -t aurora/ssh-agent-python:latest .
 docker-compose down
 docker-compose up -d
