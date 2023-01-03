7543cff11a563bdfb4fb46ad14a37d7a76de7e3a
brottenberg <brottenberg@synamedia.com>
2023-01-03 21:15:00 +0200

    separate jenkins config to different repo

--- start_system.sh
+++ start_system.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
+pwd
+project_path=$(pwd)
+echo "project_path $project_path"
+cd ..
+pwd
 git clone https://github.com/bennyrottenberg/jenkins_configuration
+cd $project_path
+pwd
 docker build -t aurora/ssh-agent-python:latest .
 docker-compose down
 docker-compose up -d
