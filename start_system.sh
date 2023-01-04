0a6f8d13fd0640e246d92cba40763b907af00d0a
bennyrottenberg <33035398+bennyrottenberg@users.noreply.github.com>
2023-01-04 15:47:55 +0200

    Update start_system.sh

--- start_system.sh
+++ start_system.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+else
+    echo "AGENT_NAME=ssh-agent-python" > .env
+    echo "AGENT_TAG=latest" >> .env
+    echo "JENKINS_TAG=lts" >> .env
+fi
+cat .env
 project_path=$(pwd)
 cd ..
 git clone https://github.com/bennyrottenberg/jenkins_configuration
 cd $project_path
-docker build -t aurora/ssh-agent-python:latest .
+echo "build agent docker ..."
+docker build -t aurora/${AGENT_NAME}:${AGENT_TAG} .
 docker-compose down
+echo "run docker-compose up -d .."
 docker-compose up -d
